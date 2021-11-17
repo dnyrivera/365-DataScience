75536e77263bc6e4787f57dc0d1093bc703cae7d
Rivera <donny.rivera@gmail.com>
2021-11-17 17:26:56 -0500

    Variables and Strings

---
+++ 03_PYTHON_BOOTCAMP/03_04-Variables_And_Strings/4_5_Exersises.py
@@ -0,0 +1,38 @@
+# QUESTION 01 - ASK THE USER FOR THE RADIUS OF A CIRCLE AND THEN PRINT THE AREA"""
+
+# radius = float(input("Insert the radius of the circle: "))
+# pi = 3.14159
+# area = pi * radius ** 2
+# print("The area of the circle is ", area)
+
+# QUESTION 02 - CONVERT FAHRENHEIT TO CELSIUS"""
+
+# far = float(input("Temperature in F : "))
+# celsisus = round((far - 32) * 5 / 9, 2)
+# print(f"The temperature {far}F in Celsius is {celsisus}")
+
+# QUESTION 03 - OBTAIN THE SUM OF TWO INTEGERS
+
+# number1 = int(input("Enter the First Number :"))
+# number2 = int(input("Enter the Second Number : "))
+# sum = number1 + number2
+# print(f"The sum between {number1} and {number2} is {sum}")
+
+# QUESTION 04 - OBTAIN THE PRODUCT OF TWO INTEGERS
+
+# number1 = int(input("Enter the First Number :"))
+# number2 = int(input("Enter the Second Number : "))
+# product = number1 * number2
+# print(f"The product between {number1} and {number2} is {product}")
+
+# QUESTION 05 - CALCULATE PIZZA
+# Bob, Ann, Jane and Ashwin want to order pizza - they know they will each eat
+# 4 slices of pizza. The local pizza shop sells pizzas of only 6 slices
+# What is the minimum number of pizzas needed - Use floor division
+
+# total_slices = 4 * 4
+# number_of_pizzas = (total_slices + 5) // 6
+# slices_left = number_of_pizzas * 6 - total_slices
+# print(
+#     f"Number of pizzas required is {number_of_pizzas}, there will be {slices_left} remaing slices."
+# )
