988a5b4282cd1b267f5b36197500cb4799d8fe4d
Rivera <donny.rivera@gmail.com>
2021-11-17 01:28:41 -0500

    Python Basics

--- 03_PYTHON_BOOTCAMP/03_03-Python_Basics/3_1_exercise-print-function-task.py
+++ 03_PYTHON_BOOTCAMP/03_03-Python_Basics/3_1_exercise-print-function-task.py
@@ -5,7 +5,12 @@ Spyder Editor
 Print function.
 """
 
-print('one',end=' ')
-print('two',end=' ')
-print('three',end=' ')
-print('four',end=' ')
+print("one", end=" ")
+print("two", end=" ")
+print("three", end=" ")
+print("four", end=" ")
+
+
+print(
+    "One, 'two', \"Three\", four, \\five\\\n\tonce,'I' caught a\ fish\n\t\t'//alive\\\\'"
+)
